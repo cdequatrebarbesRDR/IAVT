964e9802727e9ec411f5f9d4cb2f6d777520b7c9
Constance de Quatrebarbes c24b <ext.cdequatrebarbes@roederer.fr>
2025-03-11 15:12:35 +0100

    CLEANUP: remove old function search

--- database.py
+++ database.py
@@ -10,101 +10,224 @@ from models import Contrat
 from models import Document
 
 class DB:
-    def __init__(self, DB_NAME="AVENANTS"):
+    def __init__(self, DB_NAME="AVENANTS_3", reset=True):
         #self.client = AsyncMongoClient("mongodb://localhost:27017/")
         self.name = DB_NAME
         self.client = MongoClient("mongodb://localhost:27017/")
         self.db = self.client[DB_NAME]
-    
-    def init(self, contract_file="TK2501333.csv",docs_dir="./avenants_input/", results_dir="./results/", reset=True, export=True):
-        self.populate_contrats_from_csv(contract_file, reset)
-        self.populate_documents_from_fs(docs_dir, True)
+        if reset:
+            self.populate_contrats_from_csv("TK2501333.csv", reset=reset)
+        self.contrats = self.db.contrats
+        self.documents = self.db.documents
+
+    def process_from_fs(self,docs_dir="./avenants_input/", results_dir="./results/", reset=True, export=True):
+        '''From folder'''
+        self.populate_documents_from_folder(docs_dir, True)
+        
+        if export:
+            self.export_documents(docs_dir, results_dir)
+        return self
+    def stats(self):
         self.nb_contrat = self.db.contrats.count_documents({})
         self.nb_doc = self.db.documents.count_documents({}) 
-        print(f"\t-Nb contrats: {self.nb_contrat}")
         print(f"\t-Nb documents: {self.nb_doc}")
         self.nb_ref = self.db.documents.count_documents({"ref":{"$ne":None}}) 
-        self.nb_found = self.db.documents.count_documents({"found": True})
+        self.nb_poledi = self.db.documents.count_documents({"poledi":{"$ne":None}})
+        self.nb_numper = self.db.documents.count_documents({"numper":{"$ne":None}})
+        print(f"\t- Documents avec une référence de contrat dans le texte:{self.nb_ref}")
+        print(f"\t- Documents avec un numéro de contrat dans la base:{self.nb_found}")
+        print(f"\t- Documents avec un numéro de periode:{self.nb_match}")
+        self.ratio_ref = float(self.nb_ref/self.nb_doc)
+        self.ratio_poledi = float(self.nb_poledi/self.nb_ref)
+        self.ratio_numper = float(self.nb_numper / self.nb_poledi)
+        self.global_score = float(self.nb_numper / self.nb_doc)
+        print(f"% de detection du n° de contrat dans le texte : {self.ratio_ref * 100} %")
+        print(f"% de detection du n° de contrat en base : {self.ratio_poledi * 100} %")
+        print(f"% de reconciliation N° de contraty   {self.ratio_numper * 100} %")
+        print(f"Soit un % de réconciliation  {self.global_score * 100} %")
+        return self
+    
+    def process_from_db(self,reset=True, stats=True):
+        for doc in self.db.documents.find():
+            d = Document(doc)
+            #self.search_doc_in_contracts(d)
+            self.match_doc(d)    
+        print(f"\t-Nb documents: {self.nb_doc}")
+        self.nb_ref = self.db.documents.count_documents({"ref":{"$ne":None}}) 
+        self.nb_found = self.db.documents.count_documents({"poledi": True})
+        self.nb_perfect_match = self.db.documents.count_documents({"numper":{"$ne":None}})
         print(f"\t- Documents avec une référence de contrat:{self.nb_ref}")
         print(f"\t- Documents appairés:{self.nb_found}")
         self.ratio_found = float(self.nb_found/self.nb_ref)
         self.ratio_ref =float(self.nb_ref / self.nb_doc)
         self.global_score = float(self.nb_found / self.nb_doc)
         print(f"Soit un % de correspondance global de {self.global_score * 100} %")
-        if export:
-            self.export_documents(docs_dir, results_dir)
+        # if export:
+        #     self.export_documents(docs_dir, results_dir)
         return self
-    
-    def preprocess(self):
-        if self.db.contrats.count_documents({}) == 0 or self.db.contrats.count_documents({}): 
-            self.init()
-        self.group_police_number_by_cie()
-        self.group_police_edition_by_cie()
-        self.group_contrat_by_cie()
-        self.group_document_by_cie()
 
     def reset(self):
         self.db["documents"].drop()
         self.db["contrats"].drop()
+        if os.path.exists("./results"):
+            os.removedirs("./results")
+   
     
-    def search_doc_in_contracts(self, d):
-        d.found = False
-        if d.ref is None:
-            print(d.filename, d.ref, False, 0)
-            return d.found
-        
-       
-        query = {
-            "$or":[
-                {"poledi":{"$regex":d.ref.strip()}}, 
-                {"polnum":{"$regex":d.ref.strip()}}
-            ]
-        }
+    def get_police_nb(self, query, d):
+        '''Un numero de police dans la base de contrats?'''
         nb_match = self.db["contrats"].count_documents(query)
-        print(d.filename, d.ref, d.match, nb_match)
+        poledis = self.db["contrats"].find(query).distinct("poledis")
         if nb_match == 0:
-            if not hasattr(d, "original_ref") and d.cie.name == "GROUPAMA":
-                d.original_ref = d.ref
-                d.ref = d.ref.split("/")[1]
-                return self.search_doc_in_contracts(d)
-            return d.found
-        #Un seul numéro de contrat
-        poledis = self.db["contrats"].find(query).distinct('poledi')
-        numperiodes = self.db["contrats"].find(query).distinct('numper')
-        d.found = True
-        if len(poledis) == 1:
-            
-            first_match = self.db["contrats"].find_one(query) 
-            # Un seul numéro de période
-            if len(numperiodes) == 1:
-                
-                first_match = self.db["contrats"].find_one(query)
-                d.catcod = first_match['catcod']
-                d.prd = first_match['prd']
-                d.opt = first_match['opt']
-                d.fam = first_match['fam']
-                d.poledi = poledis[0]
-                d.polnum = first_match["polnum"]
-                d.entrai = first_match["entrai"]
-                d.numper = numperiodes[0]
-            else:
-                d.found = True
-                d.matches = {"count": nb_match, "poledis": poledis, "numpers": numperiodes}
-                d.poledi = poledis[0]
-                d.polnum = first_match["polnum"]
-                d.entrai = first_match["entrai"]    
-            
+            d.found = False
+            if not hasattr(d, "original_ref"): 
+                if d.cie.name == "GROUPAMA":
+                    d.original_ref = d.ref
+                    d.ref = d.ref.split("/")[1]
+                    d.matches["original_ref"] = d.original_ref
+                    return self.get_police_nb(self, query, d)
+                else:
+                    d.original_ref = d.ref
+            d.matches = {"ref": d.ref, "original_ref": d.original_match, "poledi": {"count": nb_match,"poledis": poledis}}
+            d.matches["original_ref"] = d.original_ref
+            return (d.found, d)    
         else:
-            if d.match:
-                for pedi in poledis:
-                    print(pedi, d.ref)
-                    # if pedi == d.ref:
-                    #     d.poledi = pedi
-            print(len(poledis), d.ref, poledis[0], poledis)
-
-        d.matches = {"count": nb_match, "poledis": poledis, "numpers": numperiodes}
-        return d.found
+            if not hasattr(d, "original_ref"):
+                d.original_ref = d.ref
+            d.found  = True
+            d.matches = {"ref": d.ref, "original_ref": d.original_ref, "poledi": {"count": nb_match,"poledis": poledis}}
+            if nb_match == 1:
+                d.poledi = poledis[0]        
+            else:
+                # Choose num police?
+                d.poledi = None
+            return (d.found, d)
+        
+    def get_poledi_by_ref(self, ref):
+        
+        contrat_items = self.db["contrats"].find({
+            "$or":[{"poledi": {"$regex":ref}}, {"$regex":{"polnum": ref}}]
+        })
+        police_nbs = contrat_items.distinct("poledi")
+        police_nb_count = len(police_nbs)
+        if police_nb_count == 0:
+            #pas trouvé dans la base
+            return False, None, "Pas de numéro de Contrat trouvé dans la base"
+        if police_nb_count == 1:
+            poledi = police_nbs[0]
+            return True, poledi, "Un numéro de Contrat trouvé dans la base"      
+        return False, None, f"{police_nb_count} numéro de Conntrat trouvés dans la base: lequel choisir? {police_nbs}"
+        #yield from [(True, poledi, "X numéro de Contrat") from poledi in police_nbs]
+                
+    def get_numper_catcod_fam_by_poledi(self, poledi):
+        if self.db.poledis.count_documents({"_id": poledi}) > 1:
+            raise Exception("Poledi is supposed to be unique")
+        numper_cat_codes = self.db.poledis.find_one({"_id": poledi})
+        return numper_cat_codes["periodes"]
     
+    def filter_numpers_by_catcode(self, records):
+        '''blacklist: X, K, Z'''
+        return [n for n in records if n["catcode"][0] not in ["Z", "X", "K"]]
+        
+    def get_numper_by_poledi(self, poledi):
+        numpers  = self.get_numper_catcod_fam_by_poledi(poledi)
+        if len(numpers) == 0:
+            return False, None, "No numper found for this poledi"
+        if len(numpers) == 1:
+            return True, numpers[0], "One single numper for this poledi"
+        filtered_numpers = self.filter_numpers_by_catcode(numpers)
+        if len(filtered_numpers) == 0:
+            return False, None, "No valid numper found for this poledi: {numpers}"
+        
+        if len(filtered_numpers) == 1:
+            return True, numpers[0], "One single valid numper for this poledi"
+        else:
+            
+            return False, None, "Please choose the valid numper given catcod: {filtered_numpers}"
+    
+    def match_doc(self, d):
+        d.status = False
+        d.found_ref = False
+        d.comment = "Pas de référence à un n° de contrat trouvé dans le texte"
+        if d.ref is not None:
+            d.found_ref = True
+            d.found_poledi, d.poledi, d.comment = self.get_poledi_by_ref(d.ref)
+            if d.found_poledi:
+                d.found_numper, d.numper, d.commentget_numper_by_poledi(self, d.poledi)
+                if d.found_numper:
+                    d.status = True
+                    d.new_filename = f"{d.poledi}_{d.numper}.pdf"
+                     
+        print(d.filename, d.status, d.numper, d.comment)
+        self.store_doc(d)
+        return d.status
+            
+
+
+    # def qualify_doc(self, query, d):
+    #     '''Method to match'''
+    #     nb_match = self.db["contrats"].count_documents(query)
+    #     if nb_match == 0:
+    #         d.found = False
+    #         d.numper = None
+    #         #retry for Groupama with chunk
+    #         if not hasattr(d, "original_ref") and d.cie.name == "GROUPAMA":
+    #             d.original_ref = d.ref
+    #             d.ref = d.ref.split("/")[1]
+    #             return self.qualify_doc(self, query, d)
+    #         #else simply not found
+    #         print(d.filename, ": contrat not found", d.found, "with ref", d.ref, "for CIE", d.cie.name)
+    #         return (d.found, d)
+    #     poledis = self.db["contrats"].find(query).distinct('poledi')
+    #     if len(poledis) > 1:
+    #         print("Police edition is not unique, stretching query with exact match")
+    #         stretch_query = {
+    #             "$or":[
+    #                 {"poledi":d.ref}, 
+    #                 {"polnum":d.ref}
+    #             ]
+    #         }
+    #         return self.qualify_doc(stretch_query, d)
+    #     if len(poledis) == 1:
+    #         d.found = True
+    #         catcodes = self.db["contrats"].find(query).distinct('catcod')
+    #         fams = self.db["contrats"].find(query).distinct('fam')
+    #         d.matches = {"count": nb_match, "poledis": poledis, "numpers": numperiodes, "catcods": catcodes, "fams": fams}
+    #         numperiodes = self.db["contrats"].find(query).distinct('numper')
+    #         if len(numperiodes) == 1:
+    #             first_match = self.db["contrats"].find_one(query)
+    #             d.catcod = first_match['catcod']
+    #             d.prd = first_match['prd']
+    #             d.opt = first_match['opt']
+    #             d.fam = first_match['fam']
+    #             d.poledi = poledis[0]
+    #             d.polnum = first_match["polnum"]
+    #             d.entrai = first_match["entrai"]
+    #             d.numper = numperiodes[0]
+    #             print(d.filename, ": contrat found", d.found, "with ref", d.ref, "for CIE", d.cie.name, "with numper", d.numper, d.catcod, d.fam)
+    #             return (self.found, d)
+    #         else:
+
+
+    #         d.poledi = poledis[0]
+    #         d.polnum = first_match["polnum"]
+    #         d.entrai = first_match["entrai"]
+    #         d.numper = None
+    #         print(d.filename, ": contrat found", d.found, "with ref", d.ref, "for CIE", d.cie.name, "with numper to choose with catcod and fam", catcodes, fams)
+    #         return (self.found, d)
+            
+
+    # def search_doc_in_contracts(self, d):
+    #     d.found = False
+    #     if d.ref is None:
+    #         return (d.found, d)
+    #     query =  {
+    #             "$or":[
+    #                 {"poledi":{"$regex":d.ref}}, 
+    #                 {"polnum":{"$regex":d.ref}}
+    #             ]
+    #         }
+    #     return self.qualify_doc(query, d)
+        
     def store_doc(self, d, reset=False):
         if reset:
             self.db["documents"].insert_one(d.__document__())
@@ -116,38 +239,21 @@ class DB:
             self.db["documents"].insert_one(d.__document__())
         return d
     
-    def populate_documents_from_fs(self, input_dir="./avenants_input/", reset = False):
-        print(f"INSERTING DOCS from {input_dir} INTO DB")
-        ok_dir = "./results/OK/"
-        ko_dir = "./results/KO/"
-        if not os.path.exists(ok_dir):
-            os.makedirs(ok_dir)
-        if not os.path.exists(ko_dir):
-            os.makedirs(ko_dir)
+    def populate_documents_from_folder(self, input_dir="./avenants_input/", reset = False):
+        print(f"INSERTING DOCS from {input_dir} INTO DB and FOLDERS")
         if reset:
             self.db.documents.drop()
         for filepath in glob(os.path.join(input_dir, '**', '*.pdf'), recursive=True):
             #if still ALLIANZ in documents
             if not "AZ" in filepath:
                 d = Document(filepath)
-                
-                self.search_doc_in_contracts(d)
-                self.store_doc(d)
+                self.match_doc(d)
+                # self.search_doc_in_contracts(d)
+                # self.store_doc(d)
 
-                if d.found:
-                    print(d.cie.name, d.input_filepath, d.ref, d.found)
-                    output_pdf = os.path.join(ok_dir, d.filename)
-                    output_txt = os.path.join(ok_dir, d.filetxt)
-                    shutil.copy(d.input_filepath, output_pdf)
-                    shutil.move(d.filetxt, output_txt) 
-                else:
-                    output_pdf = os.path.join(ko_dir, d.filename)
-                    output_txt = os.path.join(ko_dir, d.filetxt)
-                    shutil.copy(d.input_filepath, output_pdf)
-                    shutil.move(d.filetxt, output_txt)
-                    print(d.cie.name, d.input_filepath, d.ref, d.found)
                 
-    def export_documents(self, input_dir="./avenants_input/", output_dir= "./results/"):
+                
+    def export_documents_from_fs(self, input_dir="./avenants_input/", output_dir= "./results/"):
         for filepath in glob(os.path.join(input_dir, '**', '*.pdf'), recursive=True):
             #if still ALLIANZ in documents
             if not "AZ" in filepath:
@@ -222,6 +328,21 @@ class DB:
                     except Exception as e:
                         print(i+1, e)
                         continue
+        self.nb_contrat = self.db.contrats.count_documents({})
+        print(f"\t-Nb periodes de contrats: {self.nb_contrat}")
+        self.db.contrats.aggregate([
+            { "$group" : 
+                { 
+                    "_id" : "$poledi", 
+                    "periodes": {"$push": {"numper": "$numper", "fam": "$fam", "catcod": "$catcod"}} 
+                }
+            },
+            { "$out" : "poledis" }
+        ])
+        self.db.poledis = self.db["poledis"]
+        self.nb_police = self.db.poledis.count_documents({})
+        print(f"\t-Nb de contrats: {self.nb_contrat}")
+        return self
     
     def get_distinct_police_editions(self):
         return self.db.contrats.distinct("poledi")      
@@ -261,25 +382,26 @@ class DB:
         ])
         return self.db.cie_documents.find({})
     
+    def search_numper_by_poledi(self, poledi: str):
+        return self.db.poledis.find({"_id": poledi})
     
-            
+    def get_catcodes_by_poledi(self, poledi:str):
+        numpers = self.search_numper_by_poledi(poledi)
+        if len(numpers) is not None:
+            return {
+                poledi:[
+                    {
+                    "numper":numper, 
+                    "catcodes":self.db.contrats.find({"numper": numper}).distinct('catcod')
+                    }
+                    for numper in numpers
+                ]
+                }
+
+    def get_numper_by_catcode_and_poledi(self, poledi:str, catcod:str ):
+        return self.db.contrats.find({"poledi": poledi, "catcod":catcod}) 
 
 if __name__ == "__main__":
-    db = DB("AVENANTS_2")
-    # db.get_ref_not_found_in_docs_by_cie()
-    # db.reset()
-    db.init(contract_file="TK2501333.csv",docs_dir="./avenants_input/", reset=True, export=False)
-    # db.preprocess()
-    # db.populate_documents_from_fs("./avenants_input/")
-    # db.group_document_by_cie()
-    # db.group_contrat_by_cie()
-    # print(db.db['documents'].find_one())
-    # print(db.db['cie_contrats'].distinct("_id"))
-    # print(db.db['cie_contrats'].find_one({"_id": "HUMANIS"}))
-    # print(db.db['cie_documents'].find_one({"_id": "MUTUELLE GENERALE"}))
-    # db.matching_cies()
-    # db.get_contrats_by_cie("UNIPREVOYANCE")
-    #db.get_document_by_cie("UNIPREVOYANCE")
-    # for cie in db.db.cie_documents.dictinct("_id"):
-    #     print(cie)
+    db = DB("AVENANTS_CONTRATS", True)
+    db.process_from_fs()
     
